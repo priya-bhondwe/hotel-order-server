498fcac6999f137b5557121ebadee89e5b4f7d54
priya-bhondwe <149312604+priya-bhondwe@users.noreply.github.com>
2024-04-04 09:33:46 +0530

    routes changes

--- v1/routes/table.route.js
+++ v1/routes/table.route.js
@@ -9,8 +9,14 @@ const {
 } = require("../controllers/table.controller");
 const authorize = require("../helpers/middlewares/authorization");
 
-router.post("/", authorize(["superadmin"]), handleCreate);
-router.put("/:id", authorize(["superadmin"]), handleUpdate);
+router.post(
+  "/",
+  authorize(["superadmin"]),
+  handleCreate
+);
+router.put("/:id",
+  authorize(["superadmin"]),
+  handleUpdate);
 router.delete("/:id", authorize(["superadmin"]), handleDelete);
 router.get("/:id", authorize(["superadmin"]), handleGetOne);
 router.get("/", handleGetAll);
